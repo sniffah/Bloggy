45cd0215b142cf954a506f37f419a37bd8abc285
Ian Johannesen <ij@perlpimp.dk>
2010-11-26 21:29:17 +0100

    Added Foo Bar user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
+puts 'EMPTY THE MONGODB DATABASE'
+Mongoid.master.collections.reject { |c| c.name == 'system.indexes'}.each(&:drop)
+puts 'SETTING UP DEFAULT USER LOGIN'
+user = User.create! :name => 'Foo Bar', :email => 'foo@bar.ru', :password => 'fuckme', :password_confirmation => 'fuckme'
+puts 'New user created: ' << user.name
