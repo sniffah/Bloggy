7352599ad039992094d82f5db16ba779067a05a6
Ian Johannesen <ij@perlpimp.dk>
2010-11-26 22:51:52 +0100

    Added demo of custom mapping

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
-Bloggy::Application.routes.draw do
+Bloggy::Application.routes.draw do |map|
   root :to => "home#index"
   devise_for :users
   resources :users, :only => :show
+  #map.connect 'foo', :controller => "home", :action => "index"
 end
