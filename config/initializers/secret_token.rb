2b9890c6e757e2eccf3227840748df2df2b8c0f4
Ian Johannesen <ij@perlpimp.dk>
2010-11-26 20:54:58 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bloggy::Application.config.secret_token = 'bae53fc581b42053ccfadba0f1fbd30aaba54ad344c8c153f0d2fd4d1272dc90cd11d36a87a269f07f74fe1564780d6815251b1d688beaddf3dc6af64b62ca3e'
